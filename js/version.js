6c45a70fdca065a2f1de33cf94c9adeb2dd36ba8
stewartlittlesbrother-wq <stewartlittlesbrother@gmail.com>
2026-08-16 18:37:20 -0400

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -6,7 +6,7 @@
 // build - hard-refresh (Ctrl+Shift+R) or check the upload actually went through.
 //
 // ⬇ BUMP THIS ON EVERY CHANGE ⬇
-const APP_VERSION = 96;
+const APP_VERSION = 97;
 
 (function showAppVersion() {
     const paint = () => {
